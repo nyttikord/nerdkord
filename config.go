f6ad6a4f719a11f826e9adaf0785c7b2e940aec6
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-06-18 14:39:05 +0200

    feat(config): support postgres

--- config.go
+++ config.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/anhgelus/gokord"
 	"github.com/pelletier/go-toml/v2"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -19,11 +21,38 @@ type SQLiteConfig struct {
 	Path string `toml:"path"`
 }
 
-type PostgresConfig struct {
+func (s *SQLiteConfig) Connect() (*gorm.DB, error) {
+	return gorm.Open(sqlite.Open(s.Path), &gorm.Config{})
 }
 
-func (c *Config) Connect() (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(c.SQLite.Path), &gorm.Config{})
+func (s *SQLiteConfig) SetDefaultValues() {
+	s.Path = "nerdkord.db"
+}
+
+type PostgresConfig struct {
+	Host     string `toml:"host"`
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+	DBName   string `toml:"db_name"`
+	Port     int    `toml:"port"`
+}
+
+func (p *PostgresConfig) generateDsn() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/Paris",
+		p.Host, p.User, p.Password, p.DBName, p.Port,
+	)
+}
+
+func (p *PostgresConfig) Connect() (*gorm.DB, error) {
+	return gorm.Open(postgres.Open(p.generateDsn()), &gorm.Config{})
+}
+
+func (p *PostgresConfig) SetDefaultValues() {
+	p.Host = "localhost"
+	p.User = ""
+	p.Password = ""
+	p.DBName = ""
+	p.Port = 5432
 }
 
 func (c *Config) IsDebug() bool {
@@ -39,14 +68,16 @@ func (c *Config) GetRedisCredentials() *gokord.RedisCredentials {
 }
 
 func (c *Config) GetSQLCredentials() gokord.SQLCredentials {
-	return c
+	if c.UsePostgres {
+		return c.Postgres
+	}
+	return c.SQLite
 }
 
 func (c *Config) SetDefaultValues() {
 	c.Debug = false
 	c.Author = "nyttikord"
 	c.UsePostgres = false
-	c.SQLite = &SQLiteConfig{"nerdkord.db"}
 }
 
 func (c *Config) Marshal() ([]byte, error) {
